557f272de22a11cddbe9a4ceadc6f1940fe7a08d
JulianoAlfredo <juliano.alfredo86@gmail.com>
2026-03-04 12:38:37 -0300

    removing ip

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -8,48 +8,7 @@ import (
   "net"
 	"github.com/gin-gonic/gin"
 )
-func getLocalIPs() []string {
-    var ips []string
 
-    ifaces, err := net.Interfaces()
-    if err != nil {
-        return ips
-    }
-
-    for _, iface := range ifaces {
-        // ignora interface down e loopback
-        if (iface.Flags&net.FlagUp) == 0 || (iface.Flags&net.FlagLoopback) != 0 {
-            continue
-        }
-
-        addrs, err := iface.Addrs()
-        if err != nil {
-            continue
-        }
-
-        for _, addr := range addrs {
-            var ip net.IP
-            switch v := addr.(type) {
-            case *net.IPNet:
-                ip = v.IP
-            case *net.IPAddr:
-                ip = v.IP
-            }
-            if ip == nil || ip.IsLoopback() {
-                continue
-            }
-
-            ip = ip.To4()
-            if ip == nil {
-                continue // só IPv4
-            }
-
-            ips = append(ips, ip.String())
-        }
-    }
-
-    return ips
-}
 func main() {
 	omieService := services.NewOmieService(
 		// "7273681392978", PRODUCAO
@@ -79,15 +38,8 @@ func main() {
 
 
 	fmt.Println("Rodando na porta 8080")
-	// ...existing code...
     port := "8080"
 
-    ips := getLocalIPs()
-    for _, ip := range ips {
-        fmt.Printf("API disponível em: http://%s:%s\n", ip, port)
-    }
-
-    // ex.: gin
     if err := router.Run("0.0.0.0:" + port); err != nil {
         log.Fatal(err)
     }
