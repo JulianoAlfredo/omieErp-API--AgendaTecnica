3596edb4ac22c17d93c58cac4b53c85f1b7b73eb
JulianoAlfredo <juliano.alfredo86@gmail.com>
2026-04-16 10:34:44 -0300

    FIX: Add a logs because the logic dont alter the database

--- internal/repositories/repositoryAgenda.repositories.go
+++ internal/repositories/repositoryAgenda.repositories.go
@@ -178,6 +178,7 @@ func InserirLogFaturamento(db *sql.DB, codIntOS string, etapa string, status str
 
 func UpsertNFSEGerada(db *sql.DB, nCodNF int64, codigoOs float64, cDataEmissao string, cXmlNFSe string, cUrlNFSe string, cLinkPortal string, cNumNFSe string) (sql.Result, error) {
 	var count int
+	fmt.Printf("%f\n", codigoOs)
 	err := db.QueryRow("SELECT COUNT(1) FROM amm_contas_omie_x_agenda WHERE id_os = ?", codigoOs).Scan(&count)
 	if err != nil {
 		log.Printf("Erro ao verificar se o registro existe: %v", err)
@@ -189,11 +190,14 @@ func UpsertNFSEGerada(db *sql.DB, nCodNF int64, codigoOs float64, cDataEmissao s
 			"INSERT INTO amm_contas_omie_x_agenda (id_nf, id_os, data_emissao, xml_nfe, link_portal, numero_nf) VALUES (?, ?, ?, ?, ?, ?)",
 			nCodNF, codigoOs, cDataEmissao, cXmlNFSe, cLinkPortal, cNumNFSe,
 		)
+		fmt.Printf("inserindo novo registro")
 	} else {
 		result, err = db.Exec(
 			"UPDATE amm_contas_omie_x_agenda SET id_nf = ?, data_emissao = ?, xml_nfe = ?, link_portal = ?, numero_nf = ? WHERE id_os = ?",
 			nCodNF, cDataEmissao, cXmlNFSe, cLinkPortal, cNumNFSe, codigoOs,
 		)
+		fmt.Printf("atualizando registro existente\n")
+
 	}
 
 	if err != nil {
